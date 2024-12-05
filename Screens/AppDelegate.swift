d51c650ae0c5974599f83b24a5bb5b1a7c9e6272
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-05 11:44:30 +0200

    first commit

---
+++ Screens/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Screens
+//
+//  Created by Ahmed El Gndy on 05/12/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
